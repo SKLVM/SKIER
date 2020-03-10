30f24669a9257ace486d0f021204ac791ca72ac4
Sokolov Fedor <sokolov.noth@gmail.com>
2020-03-10 20:37:54 +0100

    Initial Commit

---
+++ SKIER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SKIER
+//
+//  Created by MAC on 10/03/2020.
+//  Copyright © 2020 cazakh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
