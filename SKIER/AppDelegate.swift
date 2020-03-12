64ab54ac2f9dfe6407a8f48e02b26d2f6c1e0532
Sokolov Fedor <sokolov.noth@gmail.com>
2020-03-12 01:06:47 +0100

    aaaaa

--- SKIER/AppDelegate.swift
+++ SKIER/AppDelegate.swift
@@ -8,14 +8,24 @@
 
 import UIKit
 import CoreData
-
+import CoreLocation
+import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
+    //FIREBASE IMPORT BLYAT
+//    func application(_ application: UIApplication,
+//        didFinishLaunchingWithOptions launchOptions:
+//        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        FirebaseApp.configure()
+//        return true
+//      }
+//    //FIREBASE IMPORT END BLYATTTTTTTTTTTTTTTTTTTTTTTT
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
--- SKIER/SceneDelegate.swift
+++ SKIER/SceneDelegate.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
-
+import Firebase
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    //FirebaseApp.configure()
+    //FIREBASE IMPORT BLYAT
+    func application(_ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
+      }
+    //FIREBASE IMPORT END BLYATTTTTTTTTTTTTTTTTTTTTTTT
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
